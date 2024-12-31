0f35abd8da3194c25eb4c34ec8340131341d0488
Lee KyuHwan <93482597+NonamedBread@users.noreply.github.com>
2025-01-01 01:56:37 +0900

    feat: HelloWorldScreen에서 View를 추가하고 텍스트를 수정하여 화면 구조를 개선, 안드로이드 번들 삭제

--- 02_MyFirstApp/src/presentation/screens/HelloWorldScreen.tsx
+++ 02_MyFirstApp/src/presentation/screens/HelloWorldScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const HelloWorldScreen = () => {
     return (
-            <Text >Hello World!</Text>
+        <View>
+            <Text>Hello World!dddddddafdsadsf11dddafd</Text>
+        </View>
     );
 };
 
